6b8bf729682dd7c71ee16c34b14e2b4f31364dec
Jomon Vincent <jomonvincent6@gmail.com>
2025-06-10 02:56:06 +0530

    procedure to insert

--- insert_procedure.sql
+++ insert_procedure.sql
@@ -1,26 +1,18 @@
-create table instructor(ins_id number primary key, ins_name varchar(100),dep_name varchar(100));
-insert into instructor(ins_id,ins_name,dep_name) values
-(10,'jomon','maths'),
-(11,'rahul','commerce'),
-(12,'adwaith','maths'),
-(13,'bavin','commerce'),
-(14,'thanzir','maths');
+create table student(roll_no number,name varchar(100));
 
-create or replace procedure ins_count(dep varchar) as pcount number;
+create or replace procedure insert_student(roll number,sname varchar) as 
 begin
-  select count(*) into pcount from instructor where dep_name=dep;
-  dbms_output.put_line(dep||' count = '||pcount);
-  exception
-    when no_data_found then
-      dbms_output.put_line('no instructor found for department '||dep);
-    when others then
-        dbms_output.put_line('Error '||sqlerrm);
-end;  
+  insert into student(roll_no,name) values(roll,sname);
+  commit;
+  dbms_output.put_line('inserted successfully');
+  dbms_output.put_line('roll no : '||roll||'  name : '||sname);
+end;
 /
 
 declare
-  dep varchar(100):='maths';
+  roll number:=&roll;
+  name varchar(100):=&name;
 begin
-  ins_count(dep);
+  insert_student(roll,name);
 end;
 /
